135d00758edd57d74cb6442bebdb8aa3a0819a79
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-07-02 17:52:50 -0300

    criando tela de projetos

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ window.addEventListener("load", function() {
   var initialItem = document.querySelector("nav ul li:first-child a");
   highlightItem(initialItem);
 });
+
+
+
+window.addEventListener('scroll', function() {
+  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrollProgress = (scrollTop / scrollHeight) * 100;
+  
+  const progressBar = document.querySelector('.progress-bar');
+  progressBar.style.width = scrollProgress + '%';
+});
