6fe124ea11613241e17d976d4bd5fbd6c4487ef9
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-07-04 15:36:35 -0300

    criando animacao aos objetos

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ window.addEventListener("load", function() {
   highlightItem(initialItem);
 });
 
+/*======= scroll progresso bar ========*/
 
 
 window.addEventListener('scroll', function() {
@@ -22,4 +23,56 @@ window.addEventListener('scroll', function() {
   
   const progressBar = document.querySelector('.progress-bar');
   progressBar.style.width = scrollProgress + '%';
+});
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+/*======= scroll sections nav-selected ========*/
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if(top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('nav-selected');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('nav-selected');
+      })
+    };
+  });
+};
+
+/*======= Animações========*/
+
+ScrollReveal({ 
+  // reset: true,
+  distance: '160px',
+  duration: 1500, 
+  delay: 100
+});
+
+ScrollReveal().reveal('.container_Formation, .top', { origin: 'top' });
+ScrollReveal().reveal('.container_Professional, .bottom',  { origin: 'bottom' });
+ScrollReveal().reveal('.right', { origin: 'left' });
+ScrollReveal().reveal('.left, .container_Contact', { origin: 'right' });
+
+
+const typed = new Typed('.multiple-text', {
+  strings: ['Desenvolvedor Front-end', ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true
+});
+
+const typedMe = new Typed('.multiple-text-2', {
+  strings: ['me!', ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 500,
+  loop: true
 });
